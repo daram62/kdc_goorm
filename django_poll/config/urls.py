1bc6002635b5b122a3bad71361f949e327966004
daram62 <97465078+daram62@users.noreply.github.com>
2024-08-28 01:33:15 +0900

    project setting

--- django_poll/config/urls.py
+++ django_poll/config/urls.py
@@ -9,13 +9,15 @@ Function views
 Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
+Including another URLconf (== urls.py)
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # "127.0.0.1:8000/polls/" 이후의 URL은 polls/urls.py가 handling
+    path('polls/', include('polls.urls')),
 ]
