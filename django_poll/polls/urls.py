642c792d07d65747f5a19115b5c33bdbf7252ab0
daram62 <97465078+daram62@users.noreply.github.com>
2024-08-28 03:17:46 +0900

    url-reversing

--- django_poll/polls/urls.py
+++ django_poll/polls/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views # 같은 폴더 내의 views.py를 import 
 
+app_name = 'polls'
+
 urlpatterns = [
     # "127.0.0.1:8000/polls/" 이후의 URL은 polls/urls.py가 handling하도록 만들 예정
     path('', views.index, name='index'), # '127.0.0.1:8000/polls/' 를 받아내도록 만들어줌
